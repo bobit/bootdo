ac43014e69845114b6fd8774029ee6b225811722
Bobit <bobit.zhang@gmail.com>
2017-12-25 14:23:18 +0800

    Update GenUtils.java
    修改生成代码所在文件夹与包名不同问题

--- bootdo/src/main/java/com/bootdo/common/utils/GenUtils.java
+++ bootdo/src/main/java/com/bootdo/common/utils/GenUtils.java
@@ -127,7 +127,7 @@ public class GenUtils {
 
             try {
                 //添加到zip
-                zip.putNextEntry(new ZipEntry(getFileName(template, tableDO.getClassname(), tableDO.getClassName(), config.getString("package").substring(config.getString("package").lastIndexOf(".") + 1))));
+                zip.putNextEntry(new ZipEntry(getFileName(template, tableDO.getClassname(), tableDO.getClassName(), config.getString("package"))));
                 IOUtils.write(sw.toString(), zip, "UTF-8");
                 IOUtils.closeQuietly(sw);
                 zip.closeEntry();
